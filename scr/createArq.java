40562f7d0dc73d7fea25ef54b122e2277c90e791
Pablo Bento Soares <pbs.97@outlook.com>
2022-08-22 14:33:00 -0300

    Criando arquivo .txt e editando o mesmo com Java.

--- scr/createArq.java
+++ scr/createArq.java
@@ -6,12 +6,15 @@ public class createArq {
 
         try {
             String name = "Curso.txt";
+
+            //Declarando metodos de criação e escrita de arquivo
             OutputStream nameArq = new FileOutputStream(name);
             Writer writer = new OutputStreamWriter(nameArq);
             BufferedWriter arqWriter = new BufferedWriter(writer);
 
             System.out.println("Criando e gravando aquivo...");
 
+            //Inserindo String no arquivo .txt Criado
             arqWriter.write("Nome: Pablo Bento Soares.");
             arqWriter.newLine();
             arqWriter.write("Curso: Boas Praticas no Desenvolvimento de Software.");
--- scr/testeMain.java
+++ scr/testeMain.java
@@ -1,11 +1,12 @@
 package scr;
 import java.io.*;
 
-public class testeMain throws IOException{
-    public static void main(String[] args)  {
+public class testeMain{
+    public static void main(String[] args) throws IOException{
 
         createArq arq = new createArq();
 
+        //Chama função reposavel por criar e escrever no arquivo.txt
         arq.Arq();
     }
 }
